4c9fefea9578eeca3329bc481ab4364926a58028
Kishore Sinha <kishore_sinha@apple.com>
2015-05-27 00:36:53 -0700

    Added code for palindrome check

---
+++ JavaCodeProgramExamples/src/com/sinha/tutorial/java/program/CheckPalindromForNumberOrString.java
@@ -0,0 +1,28 @@
+package com.sinha.tutorial.java.program;
+
+public class CheckPalindromForNumberOrString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		checkPalindromeForNumber(12521);
+	}
+	
+	public static void checkPalindromeForNumber(int number) {
+		
+		int localNum = number;
+		int reverse = 0, remainder;
+		
+		while(number > 0) {
+			remainder = number % 10;
+			reverse = reverse * 10 + remainder;
+			number = number / 10;
+		}
+		
+		if(localNum == reverse)
+			System.out.println(localNum +" is Palindrome Nummber");
+		else
+			System.out.println(localNum +" is NOT a Palindrome Nummber");
+	}
+
+}
